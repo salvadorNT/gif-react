c3a0f3aa689b53ce9afa9f1d87a6640f6efe5f1f
salvador <salvadorjnt@gmail.com>
2024-06-05 17:36:28 -0600

    render gifs and titles in GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import { getSearchGif } from "../helpers/getGifs"
 
-export const GifGrid = ({category}) => {
-  return (
-    <>
-        <h3>{category}</h3>
-        <p>{category}</p>
-    </>
-  )
+export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getSearchGif(category);
+        setImages(newImages);
+    }
+
+    useEffect(() => {
+        getImages();
+    }, [])
+
+
+    return (
+        <>
+            <h3>{category}</h3>
+            <ol>
+                {images.map(({id, title, url}) => (
+                    <>
+                        <li key={id}>{title}</li>
+                        <img src={url} alt={title} />
+                    </>
+                ))}
+            </ol>
+        </>
+    )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+
+
+export const getSearchGif = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=HPIqf77F7ZATarKhGlcySo1kjfK2R2wu&q=${category}&limit=10`
+    const response = await fetch(url);
+    const { data } = await response.json();
+    
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    console.log(gifs);
+    return gifs;
+}
