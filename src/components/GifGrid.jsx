a6f3d4d9345d294998420c5423b108cabf449f1d
salvador <salvadorjnt@gmail.com>
2024-06-05 18:16:55 -0600

    created GifItem component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getSearchGif } from "../helpers/getGifs"
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
@@ -18,14 +19,14 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{category}</h3>
-            <ol>
-                {images.map(({id, title, url}) => (
-                    <>
-                        <li key={id}>{title}</li>
-                        <img src={url} alt={title} />
-                    </>
+            <div className="card-grid">
+                {images.map((image) => (
+                   <GifItem 
+                   key={image.id}
+                   {...image}
+                   />
                 ))}
-            </ol>
+            </div>
         </>
     )
 }
